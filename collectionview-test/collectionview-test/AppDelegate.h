b46dd35d8457bb700f32609d2405745ab5a643b7
Damian at mba <damian@frey.NOSPAMco.nz>
2013-07-09 09:19:23 +0200

    collection view test

---
+++ collectionview-test/collectionview-test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  collectionview-test
+//
+//  Created by Damian Stewart on 09.07.13.
+//  Copyright (c) 2013 Damian Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
